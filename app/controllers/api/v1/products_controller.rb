1c1053bc8b38219d2ca524d3aca6ee8e483f1ef4
avanegasp <arco527@gmail.com>
2018-01-30 17:29:54 -0500

    Challenge rdy

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,9 +1,25 @@
 module Api
   module V1
     class ProductsController < ApplicationController
+      protect_from_forgery with: :null_session
       def index
         @products = Product.all
         render json: @products
+      end
+
+      def create
+        product = Product.new(products_params)
+        if product.save
+          render json: product, status: 202
+        else
+          render json: { errors: product.errors }, status: 422
+
+        end
+      end
+
+      private
+      def products_params
+        params.require(:product).permit(:name, :price)
       end
     end
   end
